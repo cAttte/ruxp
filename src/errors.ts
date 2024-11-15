f77ea571b51e65fab0a9123f6b7d1f316c21ab5f
cAttte <26514199+cAttte@users.noreply.github.com>
2024-11-15 14:17:35 -0300

    ref(NoContextError): widen for hook usage

--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,13 @@
 export class NoContextError extends TypeError {
-    constructor(child: string, id: string | null, provider: string) {
-        super(`<${child}${id ? ` id="${id}"` : ""}> must be a descendant of <${provider}>.`)
+    constructor(hook: `use${string}`, provider: string)
+    constructor(child: string, id: string | null, provider: string)
+    constructor(_thing: string, _idOrProvider: string | null, definitelyProvider?: string) {
+        if (definitelyProvider) {
+            const [child, id, provider] = arguments
+            super(`<${child}${id ? ` id="${id}"` : ""}> must be a descendant of <${provider}>.`)
+        } else {
+            const [hook, provider] = arguments
+            super(`${hook}() must be used in a descendant of <${provider}>.`)
+        }
     }
 }
