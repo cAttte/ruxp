f1426614022a4c083823529c449635bdf6a89619
cAttte <26514199+cAttte@users.noreply.github.com>
2024-11-15 14:18:01 -0300

    feat: create usePanelVisible

---
+++ src/hooks/usePanelVisible.ts
@@ -0,0 +1,23 @@
+import { useContext, useEffect, useState } from "react"
+import { PanelContext } from "../components/Panel"
+import { NoContextError } from "../errors"
+import { visibilityState } from "../events/panel/visibilityChange"
+import * as events from "../events"
+
+/**
+ * **Photoshop only**. Use the visibility state of a given panel. If no ID is provided, the parent panel context is used.
+ * @returns undefined when outside of Photoshop.
+ */
+export function usePanelVisible(id?: string): boolean | undefined {
+    const panel = useContext(PanelContext)
+    if (!panel && !id) throw new NoContextError("usePanelVisible", "Panel")
+    if (!id) id = panel!.id
+
+    const [visible, setVisible] = useState(visibilityState[id])
+    useEffect(() => {
+        events.panels[id].on("visibilityChange", setVisible)
+        return () => void events.panels[id].off("visibilityChange", setVisible)
+    }, [setVisible])
+
+    return visible
+}
